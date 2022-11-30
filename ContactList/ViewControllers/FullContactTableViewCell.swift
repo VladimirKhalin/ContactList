8dab209a29596444d40ccb647e3836352fe40cbd
VK <art.nebar@mail.ru>
2022-11-30 12:55:50 +0300

    add Table View Cell

---
+++ ContactList/ViewControllers/FullContactTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FullContactTableViewCell.swift
+//  ContactList
+//
+//  Created by Vladimir Khalin on 30.11.2022.
+//
+
+import UIKit
+
+class FullContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
